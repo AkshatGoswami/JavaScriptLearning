ba36352a194119b6a942e4fcdff6c1dd85f43a5e
AkshatGoswami <91380683+AkshatGoswami@users.noreply.github.com>
2024-11-23 20:51:37 +0000

    Stack And Heap Memory

--- JS_Basics/datatypes2.js
+++ JS_Basics/datatypes2.js
@@ -13,11 +13,11 @@ const outsideTemp = null;
 
  const id = Symbol('123')
  const id2 = Symbol('123')
-console.log(id == id2);
+//console.log(id == id2);
 
-const bigNumber = 83929119105678n;
+//const bigNumber = 83929119105678n;
 
-console.log(typeof outsideTemp);
+//console.log(typeof outsideTemp);
 
 
 
@@ -31,5 +31,31 @@ const myFunction = function(){
     console.log("Hello World");
     
 }
+
+// Memory : Stack and Heap in JavaScript
+// Stack(Primitve Type) And Heap(Non Primitive Type)
+// Example Stack
+let one = "Akshat"
+let two = one
+two = "Paneer"
+console.log(one);
+console.log(two);
+// OUtput one = Akshat and two = Paneer
+//because in stack memory copy is shared among variables so, if you change one it will definately that variable only
+
+//Example Heap
+
+let userOne = {
+    email: "Akshat@google.com",
+    upi: "akshat@ybl"
+}
+let userTwo = userOne
+userTwo.email = "Sanu@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+// output UserOne = Sanu@google.com And userTwo = Sanu@google.com
+//Because in Heap Memory The refrence is shared among the variables so, thet value of both the variables is changef
+
+
 
 
